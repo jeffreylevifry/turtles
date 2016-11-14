894e6adeaf576103db1fee2525fc5fb780b7d09b
jeffrey.levi.fry <jeffrey.levi.fry@gmail.com>
2016-11-14 12:02:37 -0500

    message

--- turtles/js/main.js
+++ turtles/js/main.js
@@ -1,8 +1,8 @@
+
+/*
 $(document).ready(function() {
  $(".draggable").draggable().css("position", "absolute");
   
-
-
 })
 
 
@@ -25,19 +25,4 @@ $("#droppable").droppable({
     //dropElem was the dropped element, return true or false to accept/refuse it
   }
 });
-
-$(".draggable").throwable({
-                        containment:[0,0,500,500],
-                        drag:true,
-                        gravity:{x:0,y:0},
-                        impulse:{
-                            f:52,
-                            p:{x:1,y:1}
-                        },
-                        shape:"circle",
-                        autostart:false,
-                        bounce:0.5,
-                        damping:100,
-                        areaDetection:[[0,0,300,300]],
-                        collisionDetection: true
-                    });
+*/
